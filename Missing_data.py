1a2810bfe9cfc472b6da9f72070cdd215a239513
Rabish Ranjan <30564326+Rabishranjan08@users.noreply.github.com>
2020-04-12 20:11:43 +0530

    Add files via upload
    Checking for the missing data

---
+++ Missing_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
